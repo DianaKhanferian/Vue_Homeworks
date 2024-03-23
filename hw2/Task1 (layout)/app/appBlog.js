1f7e37c65d6b0aba5173440f03400b52c3b2761f
Diana Khanferian <118489929+DianaKhanferian@users.noreply.github.com>
2024-03-24 01:24:31 +0300

    hw2

---
+++ hw2/Task1 (layout)/app/appBlog.js
@@ -0,0 +1,61 @@
+new Vue({
+    el: '.news__items',
+    data: {
+        articles: [
+            {
+                image: "newsImg1.png",
+                imgText: "Kitchan Design",
+                itemTitle: "Let’s Get Solution For Building Construction Work",
+                itemData: "26 December,2022"
+            },
+            {
+                image: "newsImg2.png",
+                imgText: "Living Design",
+                itemTitle: "Low Cost Latest Invented Interior Designing Ideas.",
+                itemData: "22 December,2022"
+            },
+            {
+                image: "newsImg3.png",
+                imgText: "Interior Design",
+                itemTitle: "Best For Any Office & Business Interior Solution",
+                itemData: "25 December,2022"
+            },
+            {
+                image: "newsImg1.png",
+                imgText: "Kitchan Design",
+                itemTitle: "Let’s Get Solution For Building Construction Work",
+                itemData: "26 December,2022"
+            },
+            {
+                image: "newsImg2.png",
+                imgText: "Living Design",
+                itemTitle: "Low Cost Latest Invented Interior Designing Ideas.",
+                itemData: "22 December,2022"
+            },
+            {
+                image: "newsImg3.png",
+                imgText: "Interior Design",
+                itemTitle: "Best For Any Office & Business Interior Solution",
+                itemData: "25 December,2022"
+            },
+            {
+                image: "newsImg4.png",
+                imgText: "Kitchan Design",
+                itemTitle: "Let’s Get Solution For Building Construction Work",
+                itemData: "26 December,2022"
+            },
+            {
+                image: "newsImg5.png",
+                imgText: "Living Design",
+                itemTitle: "Low Cost Latest Invented Interior Designing Ideas.",
+                itemData: "22 December,2022"
+            },
+            {
+                image: "newsImg6.png",
+                imgText: "Interior Design",
+                itemTitle: "Best For Any Office & Business Interior Solution",
+                itemData: "25 December,2022"
+            },
+        ],
+    }
+});
